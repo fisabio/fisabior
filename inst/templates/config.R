7da41256b7669b64683eb8d08677e6242134752c
Carlos Vergara-Hernández <carlos.vergara@uv.es>
2017-01-16 19:27:46 +0100

    Añado un par de paquetes a config.R y datos de muestra.

--- inst/templates/config.R
+++ inst/templates/config.R
@@ -1,7 +1,8 @@
 
 pkgs <- c('dplyr', 'data.table', 'ggplot2', 'haven', 'latex2exp', 'lme4', 'mapproj',
           'maptools', 'R2WinBUGS', 'readr', 'readxl', 'rgdal', 'sp', 'stringr',
-          'tableone', 'tidyr', 'xtable', 'parallel', 'knitr', 'rmarkdown')
+          'tableone', 'tidyr', 'xtable', 'parallel', 'knitr', 'rmarkdown',
+          'spdep', 'RColorBrewer', 'gridExtra')
 pkgs <- sapply(pkgs, function(x) suppressPackageStartupMessages(require(x, character.only = T)))
 if (length(pkgs[!pkgs]) != 0) cat('No he podido cargar los paquetes:',
                                   paste(names(pkgs[!pkgs]), collapse = ', '))
