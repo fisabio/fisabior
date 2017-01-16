4c48f99b4eb54071afd6fe41d35d3daf0541fdee
Carlos Vergara-Hernández <carlos.vergara@uv.es>
2017-01-16 19:25:42 +0100

    Modifico la función para incluir datos de muestra y un directorio con shapefiles. También optimizo elementos aislados de la función.

--- R/init_proj.R
+++ R/init_proj.R
@@ -12,6 +12,10 @@
 #'   actual de trabajo, devuelto por la función getwd().
 #' @param git Valor lógico (TRUE o FALSE), indicando si se desea generar un
 #'   repositorio git asociado al proyecto.
+#' @details La función crea la estructura de directorios a partir del nombre del
+#'   proyecto empleando minúsculas, con independencia de si el usuario ha
+#'   introducido el argumento con mayúsculas: P. ej., proj_name = 'Proyecto_X'
+#'   siempre será reconocido por la función como proj_name = 'proyecto_x'.
 #' @return La función crea la siguiente estructura de directorios en
 #'   proj_dir/proj_nom/:
 #' \itemize{
@@ -19,6 +23,7 @@
 #'     \itemize{
 #'       \item brutos/
 #'       \item procesados/
+#'       \item shapefiles/
 #'   }
 #'   \item figuras/
 #'   \item informes/
@@ -47,7 +52,9 @@
 #' }
 #' @examples
 #' init_proj()
-#' init_proj(proj_nom = 'proyecto_europeo_X', proj_dir = '~/Proyectos')
+#' init_proj(proj_nom = 'proyecto_europeo_X', proj_dir = '~/proyectos')
+
+
 init_proj <- function(proj_nom = NULL, proj_dir = NULL, git = TRUE) {
   if (is.null(proj_dir))
     stop('Por favor, indica el directorio que albergará el proyecto (proj_dir).')
@@ -69,14 +76,12 @@ init_proj <- function(proj_nom = NULL, proj_dir = NULL, git = TRUE) {
              'directorio. Prueba con otro...')
       }
       dir.create(proj_dir, recursive = T)
-      proj_nom <- paste('proyecto-', format(Sys.time(), '%Y-%m-%d'), sep = '')
     }
   } else {
-    if (any(dir_info[-1] != 0)) {
+    if (any(dir_info[-1] != 0))
       stop('Revisa el directorio de proyecto que has proporcionado. ',
            'Quizá el problema sea que no existe el directorio inmediatamente superior ',
            'o alguno intermediario...')
-    }
   }
   if (is.null(proj_nom)) {
     cat('No has proporcionado un nombre para el proyecto.\n',
@@ -85,22 +90,24 @@ init_proj <- function(proj_nom = NULL, proj_dir = NULL, git = TRUE) {
     if (acto != '1') {
       stop('Por favor, proporciona un nombre para el proyecto (proj_nom).')
     } else {
-      proj_nom <- paste('proyecto-', format(Sys.time(), '%Y-%m-%d'), sep = '')
+      proj_nom <- paste0('proyecto-', format(Sys.time(), '%Y-%m-%d'))
     }
   }
-
-  if (!grepl('/$', proj_dir)) proj_dir <- paste(proj_dir, '/', sep = '')
-  proj_dir <- paste(proj_dir, proj_nom, sep = '')
+  proj_nom <- tolower(proj_nom)
+  if (!grepl('/$', proj_dir)) proj_dir <- paste0(proj_dir, '/')
+  proj_dir <- paste0(proj_dir, proj_nom)
+  if (!grepl('/$', proj_dir)) proj_dir <- paste0(proj_dir, '/')
   if (dir.exists(proj_dir))
     stop('¡El directorio que iba a crearse ya existe! Marca una ruta o nombre diferente.')
   dir.create(proj_dir)
 
-  sub_dirs <- paste(
-    proj_dir, '/',
-    c('datos/brutos', 'datos/procesados', 'figuras', 'informes/markdown', 'informes/docx',
-      'informes/odt', 'informes/latex', 'cache', 'src/bugs', 'src/cpp', 'src/jags', 'src/stan',
-      'configuracion', 'r',  'articulo/enviado', 'articulo/revision', 'articulo/proof'),
-    sep = '')
+  sub_dirs <- paste0(
+    proj_dir,
+    c(paste0('datos/', c('brutos', 'procesados', 'shapefiles')), 'figuras',
+      paste0('informes/', c('markdown', 'docx', 'odt', 'latex')), 'cache',
+      paste0('src/', c('bugs', 'cpp', 'jags', 'stan')),
+      'configuracion', 'r',  paste0('articulo/', c('enviado', 'revision', 'proof')))
+  )
   invisible(sapply(sub_dirs, dir.create, recursive = T))
 
   template_path <- system.file('templates/template.Rproj', package = 'fisabior', mustWork = TRUE)
@@ -109,23 +116,31 @@ init_proj <- function(proj_nom = NULL, proj_dir = NULL, git = TRUE) {
   config_out <- paste(readLines(config_path), collapse = '\n')
   readme_path <- system.file('templates/README.Rmd', package = 'fisabior', mustWork = TRUE)
   readme_out <- paste(readLines(readme_path), collapse = '\n')
-  writeLines(template_out, paste(proj_dir, '/', proj_nom, '.Rproj', sep = ''))
-  writeLines(config_out, paste(proj_dir, '/configuracion/config.R', sep = ''))
-  writeLines(readme_out, paste(proj_dir, '/README.Rmd', sep = ''))
-  knitr::knit(paste(proj_dir, '/README.Rmd', sep = ''),
-              paste(proj_dir, '/README.md', sep = ''), quiet = T)
+  writeLines(template_out, paste0(proj_dir, proj_nom, '.Rproj'))
+  writeLines(config_out, paste0(proj_dir, 'configuracion/config.R'))
+  writeLines(readme_out, paste0(proj_dir, 'README.Rmd'))
+  knitr::knit(paste0(proj_dir, 'README.Rmd'),
+              paste0(proj_dir, 'README.md'), quiet = T)
 
-  files <- paste('/r/', c('importar_datos.R', 'depurar_datos.R',
-                          'descriptiva.R', 'analisis.R'), sep = '')
-  invisible(sapply(files, function(x) {
-    writeLines('\nsource(configuracion/config.R)', paste(proj_dir, x, sep = ''))
+  sample_scripts <- paste0('r/', c('importar_datos.R', 'depurar_datos.R',
+                           'descriptiva.R', 'analisis.R'))
+  invisible(sapply(sample_scripts, function(x) {
+    writeLines('\nsource(configuracion/config.R)', paste0(proj_dir, x))
   }))
-
+  sample_data <- paste0('templates/',
+                        paste0('ejemplo_aragon',
+                               c('.shx', '.shp', '.prj', '.dbf')))
+  invisible(sapply(sample_data, function(x) {
+    file.copy(system.file(x, package = 'fisabior', mustWork = TRUE),
+              sub('templates/', paste0(proj_dir, 'datos/shapefiles/'), x))
+  }))
+  invisible(file.copy(system.file('templates/ejemplo_aragon.Rdata', package = 'fisabior', mustWork = T),
+                      paste0(proj_dir, 'datos/procesados/ejemplo_aragon.Rdata')))
   if (!file.exists(Sys.which('git')) && git == TRUE) {
     warning('No tienes instalado git, así que no puedo crear el repositorio.')
   } else if (git == TRUE) {
     if (git2r::in_repository(proj_dir)) {
-      warning('El exite el repositorio git: no se hace nada.')
+      warning('Ya exite el repositorio git: no se hace nada.')
     } else {
       repo <- git2r::init(proj_dir)
       git2r::add(repo = repo, path = '*')
