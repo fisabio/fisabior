cdb3e7607132ca740492a20185a68cc3ffb4c49e
Carlos Vergara-Hernández <carlos.vergara@uv.es>
2017-03-05 21:28:07 +0100

    Parche para instalar dependencias de dependencias
    Ahora se instalan las dependencias de dependencias al ejecutar
    cualquiera de los scripts que se cargan por defecto, pues en ellos hay
    una llamada para realizar un "source('config.R')".

--- inst/templates/config.R
+++ inst/templates/config.R
@@ -1,44 +1,34 @@
 
 # Carga de paquetes -------------------------------------------------------
 pkgs <- c(
-  'data.table',
-  'devtools',
-  'dplyr',
-  'fisabior',
-  'ggplot2',
-  'gridExtra',
-  'haven',
-  'httr',
-  'knitr',
-  'latex2exp',
-  'lme4',
-  'mapproj',
-  'maptools',
-  'parallel',
-  'R2WinBUGS',
-  'RColorBrewer',
-  'readr',
-  'readxl',
-  'rgdal',
-  'rmarkdown',
-  'sp',
-  'spdep',
-  'stringr',
-  'tableone',
-  'tidyr',
-  'xtable'
+  "data.table",
+  "knitr",
+  "maptools",
+  "mlr",
+  "R2WinBUGS",
+  "rgdal",
+  "rmarkdown",
+  "rms",
+  "sp",
+  "tidyverse",
+  "tufte",
+  "xtable",
+  "fisabior"
 )
-pkgs <- sapply(pkgs, function(x) suppressPackageStartupMessages(require(x, character.only = T)))
-if (length(pkgs[!pkgs]) != 0) cat('No he podido cargar los paquetes:',
-                                  paste(names(pkgs[!pkgs]), collapse = ', '))
+invisible(sapply(pkgs, function(x) {
+  if (!x %in% rownames(installed.packages()))
+    install.packages(x)
+}))
+invisible(sapply(pkgs, function(x)
+  suppressPackageStartupMessages(require(x, character.only = TRUE))
+))
+rm(pkgs)
 
 
 # Configuración de proxy (FISABIO) ----------------------------------------
-set_config(use_proxy(
-  url      = '193.145.201.49',
+httr::set_config(httr::use_proxy(
+  url      = "193.145.201.49",
   port     = 8080,
-  username = 'invitado',
-  password = 'invitado'
+  username = "invitado",
+  password = "invitado"
 ))
-
-rm(pkgs)
