1d7b6985875049ba2ec01a2e3d9ab990c979e05f
Carlos Vergara-Hernández <carlos.vergara@uv.es>
2017-04-03 12:15:24 +0200

    Elimino algunos paquetes de carga por defecto

--- inst/templates/config.R
+++ inst/templates/config.R
@@ -1,19 +1,24 @@
 
+# Configuración de proxy (FISABIO) ----------------------------------------
+httr::set_config(httr::use_proxy(
+  url      = "193.145.201.49",
+  port     = 8080,
+  username = "invitado",
+  password = "invitado"
+))
+
+
 # Carga de paquetes -------------------------------------------------------
 pkgs <- c(
-  "data.table",
   "knitr",
   "maptools",
-  "mlr",
   "R2WinBUGS",
   "RColorBrewer",
   "rgdal",
   "rmarkdown",
-  "rms",
   "sp",
   "tidyverse",
   "tufte",
-  "xtable",
   "fisabior"
 )
 invisible(sapply(pkgs, function(x) {
@@ -22,12 +27,3 @@ invisible(sapply(pkgs, function(x) {
   suppressPackageStartupMessages(require(x, character.only = TRUE))
 }))
 rm(pkgs)
-
-
-# Configuración de proxy (FISABIO) ----------------------------------------
-httr::set_config(httr::use_proxy(
-  url      = "193.145.201.49",
-  port     = 8080,
-  username = "invitado",
-  password = "invitado"
-))
