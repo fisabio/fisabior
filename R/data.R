b31f397fd962108ea5d6c0a68c19cc7ed808be3b
Carlos Vergara-Hernández <carlos.vergara@uv.es>
2017-05-15 16:43:56 +0200

    Añado opción de agradecimientos en beamer

--- R/data.R
+++ R/data.R
@@ -1,10 +1,10 @@
-#' Mortalidad por IAM en hombres en Aragón (1991-2000)
+#' @title Mortalidad por IAM en hombres en Aragón (1991-2000)
 #'
-#' Base de datos que contiene la mortalidad observada y esperada para hombres
+#' @description Base de datos que contiene la mortalidad observada y esperada para hombres
 #' por IAM en 729 municipios de Aragón entre 1991 y 2001 y la cartografía de la
 #' región.
 #'
-#' @format Un Spatial Polygons Data Frame con 729 filas y siete variables:
+#' @format Un Spatial Polygons Data Frame (clase ) con 729 filas y siete variables:
 #' \describe{
 #'   \item{area}{área del municipio}
 #'   \item{perimeter}{perímetro del municipio}
